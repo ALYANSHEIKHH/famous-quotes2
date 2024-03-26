e66c66c79c9b51d6e956e463fa27885af6cb2a5f
ALYANSHEIKHH <alyansheikhh123@gmail.com>
2024-03-26 01:33:26 +0000

    push my quotes to git hub

---
+++ quotes.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let Famous_Person = "Albert Einstein";
+let message = `${Famous_Person}once said,"A Person who never made a mistake and never tried anything new."`;
+console.log(message);
+// koi bh famous company
+let famous_Company = "Tesla";
+let company_owner = "Elon Musk";
+let message2 = `${famous_Company}, "is the world famous electric car company"`;
+let message3 = `${company_owner}, "have world famous electric car company tesla" `;
+console.log(message2);
+console.log(message3);
---
+++ quotes.ts
@@ -0,0 +1,15 @@
+let Famous_Person: string = "Albert Einstein";
+let message: string = `${Famous_Person}once said,"A Person who never made a mistake and never tried anything new."`; 
+console.log(message);
+
+// koi bh famous company
+
+let famous_Company:string = "Tesla";
+let company_owner:string = "Elon Musk";
+let message2:string = `${famous_Company}, "is the world famous electric car company"`;
+let message3:string = `${company_owner}, "have world famous electric car company tesla" `;
+
+
+console.log(message2);
+
+console.log(message3);
